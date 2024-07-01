99b8748d16c9774e867a60f4ce156a1c32f67006
Anshul Marathe <120766533+anshulmarathe3@users.noreply.github.com>
2024-07-01 22:27:14 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,53 @@
+var swiper = new Swiper(".card_slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+          },
+        1200: {
+          slidesPerView: 4,
+        },
+      },
+  });
+
+  var swiper = new Swiper(".card2_slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+          slidesPerView: 3,
+        },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    }
+  });
