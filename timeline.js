9d6bb3bfdda745c8dc38002c6bc102f969394fad
Anshul Marathe <120766533+anshulmarathe3@users.noreply.github.com>
2024-07-03 08:16:36 +0530

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -106,3 +106,22 @@ productContainers.forEach((item, i) => {
 })
 
 
+let mybutton = document.getElementById("backToTopBtn");
+
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+mybutton.onclick = function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
+
